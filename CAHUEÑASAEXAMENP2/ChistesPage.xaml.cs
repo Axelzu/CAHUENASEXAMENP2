0e0c9779afb1a11777ca15d379b68cc2b5e1b463
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-06-02 07:42:11 -0500

    creacion de la pagina chistespage en donde nos mostrara le chiste en en elcentro y un bonton donde diga otro chiste y este nos mostrara el otro chiste

---
+++ CAHUEÑASAEXAMENP2/ChistesPage.xaml.cs
@@ -0,0 +1,39 @@
+using System.Net.Http.Json;
+
+namespace CAHUEÑASAEXAMENP2;
+
+public partial class ChistesPage : ContentPage
+{
+    HttpClient client = new HttpClient();
+
+    public ChistesPage()
+    {
+        InitializeComponent();
+        CargarChiste();
+    }
+
+    private async void CargarChiste()
+    {
+        try
+        {
+            var response = await client.GetFromJsonAsync<Chiste>("https://official-joke-api.appspot.com/random_joke");
+            chisteLabel.Text = $"{response.Setup}\n\n{response.Punchline}";
+        }
+        catch
+        {
+            chisteLabel.Text = "Error al cargar el chiste.";
+        }
+    }
+
+    private void OnNuevoChisteClicked(object sender, EventArgs e)
+    {
+        CargarChiste();
+    }
+
+    public class Chiste
+    {
+        public string Type { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+    }
+}   
