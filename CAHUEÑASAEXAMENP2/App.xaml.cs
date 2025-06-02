871c6de67b4bf61058cdf9620510a12c34fb0070
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-06-02 07:13:37 -0500

    Agregar archivos de proyecto.

---
+++ CAHUEÑASAEXAMENP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CAHUEÑASAEXAMENP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
