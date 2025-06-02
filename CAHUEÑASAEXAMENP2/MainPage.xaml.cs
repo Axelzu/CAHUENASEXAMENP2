de2464fde91bb07232ee39560ed7faa3acef68e3
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-06-02 07:26:05 -0500

    creacion de la primera pagina en donde nos muestra el mainpage cn el uso de stack louyout

--- CAHUEÑASAEXAMENP2/MainPage.xaml.cs
+++ CAHUEÑASAEXAMENP2/MainPage.xaml.cs
@@ -2,23 +2,19 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnChistesClicked(object sender, EventArgs e)
         {
-            count++;
+            await Navigation.PushAsync(new ChistesPage());
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async void OnAboutClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
         }
     }
 
